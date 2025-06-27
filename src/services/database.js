d74e9f4bf4a8fce1bedfff03a341940d3a962b3e
AlvesLuis0 <alves.luis.3002@gmail.com>
2025-06-27 11:52:44 -0300

    fix: possivelmente vai corrigir problemas de 2 firebirds com versões diferentes acessando o mesmo arquivo

--- src/services/database.js
+++ src/services/database.js
@@ -1,12 +1,24 @@
 import dotenv from 'dotenv';
 import Firebird from 'node-firebird';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const STORAGE_PATH = path.resolve(__dirname, '../../storage', 'database.fdb');
+const ORIGINAL_PATH = process.env.FB_DATABASE;
+
+function createDatabaseCopy() {
+  fs.copyFileSync(ORIGINAL_PATH, STORAGE_PATH);
+}
 
 const options = {
   host: process.env.FB_HOST || 'localhost',
   port: process.env.FB_PORT || 3050,
-  database: process.env.FB_DATABASE,
+  database: STORAGE_PATH,
   user: process.env.FB_USER,
   password: process.env.FB_PASSWORD,
   lowercase_keys: true,
@@ -14,19 +26,22 @@ const options = {
   pageSize: 4096,
   retryConnectionInterval: 1000,
   blobAsText: false,
-  encoding: 'UTF8'
+  encoding: 'UTF8',
+  copyDatabase: Number(process.env.FB_COPY_DATABASE || 1)
 };
-
-const pool = Firebird.pool(5, options);
 
 export class Database {
   static async query(sql, params = []) {
+    if(options.copyDatabase) {
+      createDatabaseCopy();
+    }
+
     return new Promise((resolve, reject) => {
-      pool.get((err, client) => {
+      Firebird.attach(options, (err, db) => {
         if (err) return reject(err);
 
-        client.query(sql, params, (err, result) => {
-          client.detach();
+        db.query(sql, params, (err, result) => {
+          db.detach();
 
           if (err) return reject(err);
 
